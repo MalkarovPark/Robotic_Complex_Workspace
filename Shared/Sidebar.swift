a1b7436c67beadf898afe50cd9b4dd012b651026
MalkarovPark <karlking5656@gmail.com>
2021-10-21 16:30:23 +0300

    Added sidebar document data connections

--- Shared/Sidebar.swift
+++ Shared/Sidebar.swift
@@ -9,10 +9,12 @@ import SwiftUI
 
 struct Sidebar: View
 {
+    @Binding var document: Robotic_Complex_WorkspaceDocument
+    
     var body: some View
     {
         //Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        SidebarContent().frame(minWidth: 200, idealWidth: 250,maxWidth: 300)
+        SidebarContent(document: $document).frame(minWidth: 200, idealWidth: 250, maxWidth: 300)
     }
 }
 
@@ -20,12 +22,14 @@ struct Sidebar_Previews: PreviewProvider
 {
     static var previews: some View
     {
-        Sidebar()
+        Sidebar(document: .constant(Robotic_Complex_WorkspaceDocument()))
     }
 }
 
 struct SidebarContent: View
 {
+    @Binding var document: Robotic_Complex_WorkspaceDocument
+    
     var body: some View
     {
         List
