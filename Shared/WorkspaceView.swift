d4b9f7ddbddd4fbd6dd27994dc9d28062a2a1529
MalkarovPark <62340924+MalkarovPark@users.noreply.github.com>
2021-11-06 19:16:08 +0300

    Added toolbar to to Workspace view.

--- Shared/WorkspaceView.swift
+++ Shared/WorkspaceView.swift
@@ -13,11 +13,53 @@ struct WorkspaceView: View
     
     var body: some View
     {
+        #if os(macOS)
+        let placement_trailing: ToolbarItemPlacement = .automatic
+        #else
+        let placement_trailing: ToolbarItemPlacement = .navigationBarTrailing
+        #endif
+        
         TextEditor(text: $document.text)
         #if os(iOS)
             .padding()
         #endif
-        //Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        .toolbar
+        {
+            ToolbarItem(placement: placement_trailing) //.principal)
+            {
+                HStack(alignment: .center)
+                {
+                    Button(action: add_robot)
+                    {
+                        Label("Robots", systemImage: "stop")
+                    }
+                    Button(action: add_robot)
+                    {
+                        Label("Robots", systemImage: "play")
+                    }
+                    Divider()
+                    Button(action: add_robot)
+                    {
+                        Label("Robots", systemImage: "arrow.uturn.backward")
+                    }
+                    Button(action: add_robot)
+                    {
+                        Label("Robots", systemImage: "arrow.uturn.forward")
+                    }
+                    Divider()
+                    Button(action: add_robot)
+                    {
+                        Label("Robots", systemImage: "plus")
+                    }
+                }
+            }
+        }
+    }
+    
+    func add_robot()
+    {
+        print("⚗️")
     }
 }
 
