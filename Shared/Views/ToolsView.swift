897525a68ce0a4e0ee6a0c8c4672e3dd9a7e1e79
MalkarovPark <62340924+MalkarovPark@users.noreply.github.com>
2023-03-04 19:35:23 +0300

    Provided apply bit mask fix for iOS/iPadOS.

--- Shared/Views/ToolsView.swift
+++ Shared/Views/ToolsView.swift
@@ -1143,6 +1143,7 @@ struct ToolSceneView_iOS: UIViewRepresentable
             
             scene_view.scene?.rootNode.addChildNode(app_state.previewed_object?.node ?? SCNNode())
             app_state.previewed_object?.node?.name = "Tool"
+            apply_bit_mask(node: app_state.previewed_object!.node ?? SCNNode(), Workspace.tool_bit_mask)
             app_state.preview_update_scene = false
         }
         
