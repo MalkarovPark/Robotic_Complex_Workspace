5b3d1cee53098e4af736f77533e1f5732b51254d
MalkarovPark <62340924+MalkarovPark@users.noreply.github.com>
2023-11-06 21:13:11 +0300

    The parts view updated with new ObjectSceneView component of IndustrialKit interface elements. Removed the ViewCloseButton listing file.

--- Robotic Complex Workspace/Views/PartsView.swift
+++ Robotic Complex Workspace/Views/PartsView.swift
@@ -208,7 +208,7 @@ struct AddPartView: View
     {
         VStack(spacing: 0)
         {
-            PartSceneView()
+            PreviewSceneView()
                 .overlay(alignment: .top)
                 {
                     Text("Add Part")
@@ -267,8 +267,8 @@ struct AddPartView: View
         #if os(macOS)
         .frame(minWidth: 400, idealWidth: 480, maxWidth: 640, minHeight: 400, maxHeight: 480)
         #endif
-        .onAppear()
-        {
+        .onChange(of: app_state.part_name)
+        { oldValue, newValue in
             app_state.update_part_info()
         }
     }
@@ -310,7 +310,7 @@ struct PartView: View
     {
         VStack(spacing: 0)
         {
-            PartSceneView()
+            PartSceneView(part: $part_item)
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
             
             Divider()
@@ -391,135 +391,22 @@ struct PartView: View
             {
                 document.preset.parts = base_workspace.file_data().parts
             }
-            //document.preset.parts = base_workspace.file_data().parts
             is_document_updated = true
         }
     }
 }
 
 //MARK: - Scene views
-struct PartSceneView: UIViewRepresentable
+struct PreviewSceneView: View
 {
-    @AppStorage("WorkspaceImagesStore") private var workspace_images_store: Bool = true
-    
-    @EnvironmentObject var base_workspace: Workspace
     @EnvironmentObject var app_state: AppState
     
-    let scene_view = SCNView(frame: .zero)
-    let viewed_scene = SCNScene(named: "Components.scnassets/View.scn")!
-    
-    func scn_scene(context: Context) -> SCNView
+    var body: some View
     {
-        app_state.reset_view = false
-        app_state.reset_view_enabled = true
-        scene_view.scene = viewed_scene
-        scene_view.delegate = context.coordinator
-        scene_view.scene?.background.contents = UIColor.clear
-        return scene_view
+        ObjectSceneView(scene: SCNScene(named: "Components.scnassets/View.scn")!, on_render: update_preview_node(scene_view:), on_tap: { _, _ in })
     }
     
-    #if os(macOS)
-    func makeNSView(context: Context) -> SCNView
-    {
-        base_workspace.camera_node = viewed_scene.rootNode.childNode(withName: "camera", recursively: true)
-        
-        //Add gesture recognizer
-        let tap_gesture_recognizer = UITapGestureRecognizer(target: context.coordinator, action: #selector(context.coordinator.handle_tap(_:)))
-        scene_view.addGestureRecognizer(tap_gesture_recognizer)
-        
-        scene_view.allowsCameraControl = true
-        scene_view.rendersContinuously = true
-        scene_view.autoenablesDefaultLighting = true
-        
-        scene_view.backgroundColor = UIColor.clear
-        
-        return scn_scene(context: context)
-    }
-    #else
-    func makeUIView(context: Context) -> SCNView
-    {
-        base_workspace.camera_node = viewed_scene.rootNode.childNode(withName: "camera", recursively: true)
-        
-        //Add gesture recognizer
-        let tap_gesture_recognizer = UIGestureRecognizer(target: context.coordinator, action: #selector(context.coordinator.handle_tap(_:)))
-        scene_view.addGestureRecognizer(tap_gesture_recognizer)
-        
-        scene_view.allowsCameraControl = true
-        scene_view.rendersContinuously = true
-        scene_view.autoenablesDefaultLighting = true
-        
-        scene_view.backgroundColor = UIColor.clear
-        
-        return scn_scene(context: context)
-    }
-    #endif
-    
-    #if os(macOS)
-    func updateNSView(_ ui_view: SCNView, context: Context)
-    {
-        //Update commands
-        app_state.reset_camera_view_position(locataion: base_workspace.camera_node?.position ?? SCNVector3(0, 0, 0), rotation: base_workspace.camera_node?.rotation ?? SCNVector4(0, 0, 0, 0), view: ui_view)
-        
-        if app_state.get_scene_image && workspace_images_store
-        {
-            app_state.get_scene_image = false
-            app_state.previewed_object?.image = ui_view.snapshot()
-        }
-    }
-    #else
-    func updateUIView(_ ui_view: SCNView, context: Context)
-    {
-        //Update commands
-        app_state.reset_camera_view_position(locataion: base_workspace.camera_node?.position ?? SCNVector3(0, 0, 0), rotation: base_workspace.camera_node?.rotation ?? SCNVector4(0, 0, 0, 0), view: ui_view)
-        
-        if app_state.get_scene_image && workspace_images_store
-        {
-            app_state.get_scene_image = false
-            app_state.previewed_object?.image = ui_view.snapshot()
-        }
-    }
-    #endif
-    
-    func makeCoordinator() -> Coordinator
-    {
-        Coordinator(self, scene_view)
-    }
-    
-    final class Coordinator: NSObject, SCNSceneRendererDelegate
-    {
-        var control: PartSceneView
-        
-        init(_ control: PartSceneView, _ scn_view: SCNView)
-        {
-            self.control = control
-            
-            self.scn_view = scn_view
-            super.init()
-        }
-        
-        func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval)
-        {
-            control.scene_check()
-        }
-        
-        private let scn_view: SCNView
-        @objc func handle_tap(_ gesture_recognize: UITapGestureRecognizer)
-        {
-            let tap_location = gesture_recognize.location(in: scn_view)
-            let hit_results = scn_view.hitTest(tap_location, options: [:])
-            var result = SCNHitTestResult()
-            
-            if hit_results.count > 0
-            {
-                result = hit_results[0]
-                
-                print(result.localCoordinates)
-                print("🍮 tapped – \(result.node.name!)")
-            }
-        }
-    }
-    
-    func scene_check() //Render functions
+    private func update_preview_node(scene_view: SCNView)
     {
         if app_state.preview_update_scene
         {
@@ -529,6 +416,29 @@ struct PartSceneView: UIViewRepresentable
             scene_view.scene?.rootNode.addChildNode(app_state.previewed_object?.node ?? SCNNode())
             app_state.previewed_object?.node?.name = "Figure"
             app_state.preview_update_scene = false
+        }
+    }
+}
+
+struct PartSceneView: View
+{
+    @AppStorage("WorkspaceImagesStore") private var workspace_images_store: Bool = true
+    
+    @EnvironmentObject var app_state: AppState
+    
+    @Binding var part: Part
+    
+    var body: some View
+    {
+        ObjectSceneView(scene: SCNScene(named: "Components.scnassets/View.scn")!, node: part.node ?? SCNNode(), on_render: update_view_node(scene_view:), on_tap: { _, _ in })
+    }
+    
+    private func update_view_node(scene_view: SCNView)
+    {
+        if app_state.get_scene_image && workspace_images_store
+        {
+            app_state.get_scene_image = false
+            app_state.previewed_object?.image = scene_view.snapshot()
         }
     }
 }
